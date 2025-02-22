8ba0b988c4df96cd6f60e5930edfb3b71a24b071
SimosPrks <proikakis.simos@gmail.com>
2025-02-22 21:46:21 +0100

    First commit

---
+++ human-eval/human_eval/config.py
@@ -0,0 +1,31 @@
+# If you have just one, you can also just paste one path in the list.
+MODIFIED_MODEL_PATH = [
+
+    "C:/Users/proik/Results/CodeLlama0Percent/round_1/merged_model",
+    "C:/Users/proik/Results/CodeLlama0Percent/round_2/merged_model",
+    "C:/Users/proik/Results/CodeLlama0Percent/round_3/merged_model",
+    "C:/Users/proik/Results/CodeLlama0Percent/round_4/merged_model",
+    "C:/Users/proik/Results/CodeLlama0Percent/round_5/merged_model",
+    "C:/Users/proik/Results/CodeLlama25Percent/round_1/merged_model",
+    "C:/Users/proik/Results/CodeLlama25Percent/round_2/merged_model",
+    "C:/Users/proik/Results/CodeLlama25Percent/round_3/merged_model",
+    "C:/Users/proik/Results/CodeLlama25Percent/round_4/merged_model",
+    "C:/Users/proik/Results/CodeLlama25Percent/round_5/merged_model",
+    "C:/Users/proik/Results/CodeLlama5Percent/round_1/merged_model",
+    "C:/Users/proik/Results/CodeLlama5Percent/round_2/merged_model",
+    "C:/Users/proik/Results/CodeLlama5Percent/round_3/merged_model",
+    "C:/Users/proik/Results/CodeLlama5Percent/round_4/merged_model",
+    "C:/Users/proik/Results/CodeLlama5Percent/round_5/merged_model",
+    "C:/Users/proik/Results/CodeLlama10Percent/round_1/merged_model",
+    "C:/Users/proik/Results/CodeLlama10Percent/round_2/merged_model",
+    "C:/Users/proik/Results/CodeLlama10Percent/round_3/merged_model",
+    "C:/Users/proik/Results/CodeLlama10Percent/round_4/merged_model",
+    "C:/Users/proik/Results/CodeLlama10Percent/round_5/merged_model"
+
+]
+
+BASE_GENERATED_CODE_OUTPUT_PATH = "C:/Users/proik/functionalityAnalysis/"
+
+HUMANEVAL_DATA_PATH = "C:/Users/proik/functionalityAnalysis/human-eval/data/HumanEval.jsonl.gz"
+
+
